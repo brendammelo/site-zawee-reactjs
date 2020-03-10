2e7d1337a57f331a0473a38bbcbf42de2bea3a3a
Brenda Monteiro <brenda.monteiro.melo@gmail.com>
2020-03-10 20:32:18 -0300

    components importados

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/header/HeaderComponent';
+import Motivo from './components/motivo/PorqueZawee';
+import Solucao from './components/solucoes/SolutionsComponent';
+import Impulsionar from './components/impulsionar/ImpulsionarComponent';
+import Form from './components/form/Form';
+import Footer from './components/footer/Footer';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < Header />
+      < Motivo />
+      < Solucao />
+      < Form />
+      < Footer />
+
     </div>
   );
 }
