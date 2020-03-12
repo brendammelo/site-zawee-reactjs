5c8b82e30e25edef928a60f294d4c1acc8b8f7f4
Deborah Almeida <deborahsoares01@gmail.com>
2020-03-11 21:43:48 -0300

    titulo e legenda adicionados no header

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -1,21 +1,25 @@
 
 import './headerComponent.css';
 import React from 'react';
+import '/headerComponents.css'
 
-class HeaderComponent extends React.Component {
-render(){
-return(
+const HeaderComponent = () => {
+    return (
 
-<div>
-    
-</div>
-    
-    
-    
-    
- )};
+        <section className = "section_header">
+            <div className = "menu_header"></div>
+            <div className = "hero_header">
+                <h1 className = "titulo_header">Zawee</h1>
+                <h2 className = "legenda_header">Soluções digitais para o seu negócio</h2>
+            </div>
+        </section>
 
+
+
+    )
 }
 
 
-export default HeaderComponent;
+export default HeaderComponent
+
+
