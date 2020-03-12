aca8ac90458ffb036829acdbff9fc4c16b26f834
Deborah Almeida <deborahsoares01@gmail.com>
2020-03-12 18:36:25 -0300

    centralizando itens do header

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -7,15 +7,17 @@ const HeaderComponent = () => {
 
         <section className = "section_header">
             <div className = "menu_header"></div>
-            <div className = "hero_header">
-                <h1 className = "titulo_header">Zawee</h1>
-                <h2 className = "legenda_header">Soluções digitais para o seu negócio</h2>
-            </div>
-            <div className="link_hero" href="a">
-                <p className="scroll_down"> Scroll Down </p>
-                <a className="ancora_hero" href="a">
-                    <img className="icone_hero"   />
-                </a>
+            <div className = "teste_header">
+                <div className = "hero_header">
+                    <h1 className = "titulo_header">Zawee</h1>
+                    <h2 className = "legenda_header">Soluções digitais para o seu negócio</h2>
+                </div>
+                <div className="link_hero" href="a">
+                    <p className="scroll_down"> Scroll Down </p>
+                    <a className="ancora_hero" href="a">
+                        <img className="icone_hero"   />
+                    </a>
+                </div>
             </div>
         </section>
 
