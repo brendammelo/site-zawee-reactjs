9866d4b67b791817a3239974ac9eaec536801c6e
Brenda Monteiro <brenda.monteiro.melo@gmail.com>
2020-03-13 17:04:35 -0300

    icones add, quem somos add

--- src/components/quemSomos/QuemSomos.js
+++ src/components/quemSomos/QuemSomos.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './quemSomos.css';
+import imgQuemSomos from '../../assets/icons/quem-somos.png';
 
 const QuemSomos = () => {
     return (
         <div className="container">
             <div className="title"><h2>Quem é a Zawee</h2></div>
-            <div className="content"></div>
+            <div className="content">
+                <div className="texto"><h3>Somos um grupo de alunos da UFRPE, 
+                                    buscando mudar a forma como pequenas e 
+                                    médias empresas, são vistas online</h3></div>
+                <div className="imagem"> 
+                    <img className="icone_quemSomos" 
+                        src={imgQuemSomos} 
+                        alt="desenvolvimento multi plataforma"
+                        height="50%"
+                    />
+                </div>
+            </div>
             <div className="button"></div>
         </div>
     )
