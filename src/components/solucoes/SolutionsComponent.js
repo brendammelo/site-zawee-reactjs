d08eb33878710202798b17f147ad50eaa605cbdd
Brenda Monteiro <brenda.monteiro.melo@gmail.com>
2020-03-16 18:13:47 -0300

    add img e texto solutions, correçao do scroll header

--- src/components/solucoes/SolutionsComponent.js
+++ src/components/solucoes/SolutionsComponent.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './solutionsComponent.css';
 import img_solucoes from '../../assets/imgs/img_solucoes.jpg'
 
+import websites from '../../assets/icons/websites.png';
+import redeSociais from '../../assets/icons/redes-sociais.png'
+import Seo from '../../assets/icons/seo.png';
+
 class SolutionsComponent extends Component {
 render ()  {
         
@@ -15,16 +19,30 @@ render ()  {
                 <div className="container-solutions">
     
                         <div className="box_solucoes1">
-                            <img></img>
-                            <p className="texto_solucoes1"></p>
+                            <img 
+                                style={{marginLeft:'2vw'}}
+                                src={websites} 
+                                alt="desenvolvimento de sites"
+                                height="35%"/>
+                            <p className="texto_solucoes1">Desenvolvimento de<br></br>
+                                            websites</p>
                         </div>
-                        <div className="img-box_solucoes2">
-                            <img></img>
-                            <p className="texto_solucoes1"></p>
+                        <div className="box_solucoes2">
+                            <img 
+                                style={{marginLeft:'5vw'}} 
+                                src={redeSociais} 
+                                alt="gerenciamento de redes sociais"
+                                height="35%"/>
+                            <p className="texto_solucoes1">Criação e gerenciamento <br></br>
+                                de redes sociais</p>
                         </div>
                         <div className="box_solucoes3">
-                            <img></img>
-                            <p className="texto_solucoes1"></p>
+                            <img 
+                                style={{marginLeft:'2vw'}}
+                                src={Seo} 
+                                alt="seo e google ads"
+                                height="35%"/>
+                            <p className="texto_solucoes1">Google Ads, e SEO</p>
                         </div>
                 </div>
             </div>
