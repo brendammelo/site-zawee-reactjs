e8ed83a28dd78831f2e5526f70cde530b0d5b9fc
Deborah Almeida <deborahsoares01@gmail.com>
2020-03-17 19:39:13 -0300

    add parte do arquivo motivos

---
+++ src/components/motivo/PorqueZawee.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import './porqueZawee.css';
+import  from '../../assets/imgs/'
+
+import  from '../../assets/icons/';
+import  from '../../assets/icons/.png'
+import  from '../../assets/icons/.png';
