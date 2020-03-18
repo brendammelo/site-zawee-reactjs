a17f64f87e0a8b768e51c8cb82ef5891657b0af7
Brenda Monteiro <brenda.monteiro.melo@gmail.com>
2020-03-17 22:38:50 -0300

    add icons, add fontawesome

--- src/components/diferencial/DiferencialZawee.js
+++ src/components/diferencial/DiferencialZawee.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './diferencial.css';
 
+import img1 from '../../assets/icons/prototipo.png';
+import img2 from '../../assets/icons/suporte.png';
+import img3 from '../../assets/icons/qualidade.png';
+
+
 class DiferencialComponent extends Component {
     render ()  {
             
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './form.css';
 
+import img1 from '../../assets/icons/contato.png';
+
 class Form extends Component {
     render ()  {
             
