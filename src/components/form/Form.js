4f5c09d12a6538e6fcd125434f93a0f02984be84
Brenda Monteiro <brenda.monteiro.melo@gmail.com>
2020-03-19 16:27:18 -0300

    add hubspot form

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './form.css';
+import HubspotForm from 'react-hubspot-form';
 
 import img_contato from '../../assets/icons/contato.png';
 
@@ -12,7 +13,16 @@ class Form extends Component {
             <section className="section_form">
                 <h2 className="title_form">Vem falar com a gente!</h2>
                 <div className="container_formulario">
-                    <div className="container_form"></div>
+                    <div className="container_form">
+                    <HubspotForm
+                        portalId="7337380"
+                        formId="cebf6b2a-4702-432f-83ce-b4cddc0b536e"
+                        onSubmit={() => console.log('Submit!')}
+                        onReady={(form) => console.log('Form ready!')}
+                        loading={<div>Loading...</div>}
+                    />
+
+                    </div>
                     <div className="box_img_botao">
                         <img className="img_contato" src={img_contato}/>
                         <div className="button_form">
@@ -24,7 +34,6 @@ class Form extends Component {
             </>
 
         )}}
-
 
 
     export default Form;
